61483fa690230d8d9d2b0956aac89841d08c687b
Adam Putinski <adam.putinski@gmail.com>
2014-02-05 10:26:57 -0800

    Updated build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-// LOAD ALL GRUNT TASKS
+  // LOAD ALL GRUNT TASKS
   require('load-grunt-tasks')(grunt);
 
   // Project configuration.
@@ -14,32 +14,104 @@ module.exports = function(grunt) {
           debug: true
         },
         files: {
-          'ja/app.js': ['tmp/**/*.js']
+          'production/js/app.js': ['tmp/js/app.js']
         }
       }
     },
 
-    jsx: {
+    react: {
       client: {
-        src: 'js/**/*/.jsx',
-        dest: 'tmp/**/*.js',
+        files: [{
+          expand: true,
+          cwd: 'app/js',
+          src: ['**/*.js', '**/*.jsx'],
+          dest: 'tmp/js',
+          ext: '.js'
+        }]
       },
     },
 
     compass: {
       resources: {
         options: {
-          sassDir: 'scss',
-          cssDir: 'css',
+          sassDir: 'app/scss',
+          cssDir: 'production/css',
           environment: 'production',
           outputStyle: 'expanded'
         }
       }
     },
 
-    clean: ["tmp"]
+    clean: ["tmp"],
+
+    copy: {
+      html: {
+        files: [{
+          expand: true,
+          cwd: 'app/html',
+          src: ['**/*'],
+          dest: 'production'
+        }]
+      },
+      bower: {
+        files: [{
+          expand: true,
+          cwd: '.',
+          src: ['bower_components/**/*'],
+          dest: 'production'
+        }]
+      }
+    },
+
+    connect: {
+      livereload: {
+        options: {
+          port: 8000,
+          livereload: 35729, // change this to '0.0.0.0' to access the server from outside
+          hostname: 'localhost',
+          base: 'production',
+          open: true
+        }
+      }
+    },
+
+    watch: {
+
+      options: {
+        debounceDelay: 250
+      },
+
+      compass: {
+        files: ['app/scss/**/*.scss'],
+        tasks: ['compass']
+      },
+
+      js: {
+        files: ['app/js/**/*'],
+        tasks: ['react', 'browserify']
+      },
+
+      html: {
+        files: ['app/html/**/*'],
+        tasks: ['copy:html']
+      },
+
+      livereload: {
+        options: {
+          livereload: '<%= connect.livereload.options.livereload %>'
+        },
+        files: [
+          'app/scss/**/*.scss',
+          'app/js/**/*',
+          'app/html/**/*'
+        ]
+      }
+
+    }
+
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['compass', 'jsx', 'browserify', 'clean']);
+  grunt.registerTask('default', ['compass', 'react', 'browserify', 'copy', 'clean', 'connect', 'watch']);
+
 };
---
+++ app/js/app.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+var initialize = function () {
+
+  var container = document.createElement('div');
+  container.id = 'forge';
+  document.body.appendChild(container);
+
+  React.renderComponent(
+    <h1>Hello, world!</h1>,
+    container
+  );
+
+};
+
+document.addEventListener('DOMContentLoaded', initialize);
